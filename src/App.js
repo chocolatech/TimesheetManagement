39d3c23955af4fe66cdff26da0775b1e645c6f69
ewelina <evelinagonda@gmail.com>
2017-02-15 15:40:09 +0100

    add sample nested views

--- src/App.js
+++ src/App.js
@@ -3,30 +3,44 @@ import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 're
 import './App.css';
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <Router history={hashHistory}>
-        <Route path='/' component={Home} />
-        <Route path='/address' component={Address} />
-        <Route path='*' component={NotFound} />
+        <Route path='/' component={Container}>
+          <IndexRoute component={Home} />
+          <Route path='address' component={Address}>
+            <IndexRoute component={TwitterFeed} />
+            <Route path='instagram' component={Instagram} />
+          </Route>
+          <Route path='*' component={NotFound} />
+        </Route>
       </Router>
-
-
-      // <div className="App">
-      //   <div className="App-header">
-      //     <h2>Welcome to React</h2>
-      //   </div>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
-    );
+    )
   }
 }
 
 const Home = () => <h1>Hello from Home!</h1>
-const Address = () => <h1>We are located at 555 Jackson St.</h1>
+const Address = (props) => <div>
+  <br />
+  <Link to='/address'>Twitter Feed</Link>&nbsp;
+  <Link to='/address/instagram'>Instagram Feed</Link>
+  <h1>We are located at 555 Jackson St.</h1>
+  {props.children}
+</div>
 const NotFound = () => (
   <h1>404.. This page is not found!</h1>)
+
+const Nav = () => (
+  <div>
+    <Link to='/'>Home</Link>&nbsp;
+    <Link to='/address'>Address</Link>
+  </div>)
+const Container = (props) => <div>
+  <Nav />
+  {props.children}
+</div>
+
+const Instagram = () => <h3>Instagram Feed</h3>
+const TwitterFeed = () => <h3>Twitter Feed</h3>
 
 export default App;
