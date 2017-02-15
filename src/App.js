95ecbcdf35d77fa3d59912995315fae611434505
ewelina <evelinagonda@gmail.com>
2017-02-15 14:23:05 +0100

    add react-router && basic routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router history={hashHistory}>
+        <Route path='/' component={Home} />
+        <Route path='/address' component={Address} />
+        <Route path='*' component={NotFound} />
+      </Router>
+
+
+      // <div className="App">
+      //   <div className="App-header">
+      //     <h2>Welcome to React</h2>
+      //   </div>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
     );
   }
 }
+
+const Home = () => <h1>Hello from Home!</h1>
+const Address = () => <h1>We are located at 555 Jackson St.</h1>
+const NotFound = () => (
+  <h1>404.. This page is not found!</h1>)
 
 export default App;
