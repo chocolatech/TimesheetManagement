817fd8c77ed1381897c1eb4a00d0bdabd0e8d2e2
cobbie <evelinagonda@gmail.com>
2017-02-16 20:36:21 +0100

    add working bootstrap && paper

--- src/components/topNav.component.js
+++ src/components/topNav.component.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 
 const TopNav = () => (
     <div>
         <Link to='/'>Home</Link>&nbsp;
         <Link to='/address'>Address</Link>
+        <a href="#" className="btn btn-primary">Primary</a>
     </div>
 )
 
