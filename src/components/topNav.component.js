4f92f0a119a7692061f64ac7c2a56f413bbb4dda
ewelina <evelinagonda@gmail.com>
2017-02-16 15:30:09 +0100

    material plays

--- src/components/topNav.component.js
+++ src/components/topNav.component.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router';
+import {AppBar} from 'react-toolbox/lib/app_bar';
+// import 'react-toolbox/lib/app_bar/theme.css';
+//import AppBar from 'react-toolbox/lib/app_bar/AppBar.js';
 
 const TopNav = () => (
     <div>
         <Link to='/'>Home</Link>&nbsp;
         <Link to='/address'>Address</Link>
+        <AppBar/>
     </div>
 )
 
