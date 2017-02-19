1a4521cf1b9d96cd4298c1ebe93b86f63f953e08
cobbie <evelinagonda@gmail.com>
2017-02-19 20:37:37 +0100

    App.js cleanup

--- src/App.js
+++ src/App.js
@@ -24,6 +24,6 @@ const Address = (props) => <div>
   {/*<Link to='/address'>Twitter Feed</Link>&nbsp;
   <Link to='/address/instagram'>Instagram Feed</Link>*/}
   <h1>We are located at 555 Jackson St.</h1>
-  {props.children}
+  {/*{props.children}*/}
 </div>
 export default App
