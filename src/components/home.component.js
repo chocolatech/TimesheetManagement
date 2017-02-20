77279c2421a2fc4a7a183289b52f9cff9c28fb75
ewelina <evelinagonda@gmail.com>
2017-02-20 13:11:14 +0100

    add colorfull labels to home view

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import '../styles/sideNav.css';
+import '../styles/home.css';
 
 const Home = () => (
-    <div>
-        <h1>Simple Sidebar</h1>
-        <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
-        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquet porttitor neque, ac volutpat ex convallis sed. Donec ornare, quam in porttitor luctus, velit enim aliquet mauris, in vulputate urna augue suscipit enim. Phasellus pellentesque augue vel venenatis dignissim. Quisque suscipit nunc vitae erat commodo facilisis. Proin vestibulum sem ut ligula aliquet, non sagittis nisl consectetur. Praesent tempus nunc dui, eu aliquam lorem efficitur a. Phasellus laoreet justo ut justo ultricies, vel accumsan eros cursus. Curabitur rutrum turpis tortor, id rutrum est tincidunt vel. Vivamus non dictum leo. Donec vel velit ante. Integer vehicula metus metus, in scelerisque quam fringilla vitae. Etiam quis ullamcorper dolor, in lacinia massa. Cras volutpat odio quis mi vulputate, id laoreet massa blandit. Morbi a consequat risus. Curabitur sollicitudin et diam at lacinia.
-
-Nunc congue eget risus a semper. Suspendisse egestas vulputate urna, sit amet fermentum mi efficitur in. Nulla ut nulla libero. Donec consequat placerat metus sit amet dapibus. Proin massa magna, pellentesque id luctus ut, rhoncus sed lacus. Donec fringilla imperdiet nulla, aliquet gravida erat lacinia et. Donec in commodo lectus, at ullamcorper dolor. Sed aliquet, justo non tincidunt pretium, quam mauris vulputate risus, quis euismod nunc odio non nibh.
-</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquet porttitor neque, ac volutpat ex convallis sed. Donec ornare, quam in porttitor luctus, velit enim aliquet mauris, in vulputate urna augue suscipit enim. Phasellus pellentesque augue vel venenatis dignissim. Quisque suscipit nunc vitae erat commodo facilisis. Proin vestibulum sem ut ligula aliquet, non sagittis nisl consectetur. Praesent tempus nunc dui, eu aliquam lorem efficitur a. Phasellus laoreet justo ut justo ultricies, vel accumsan eros cursus. Curabitur rutrum turpis tortor, id rutrum est tincidunt vel. Vivamus non dictum leo. Donec vel velit ante. Integer vehicula metus metus, in scelerisque quam fringilla vitae. Etiam quis ullamcorper dolor, in lacinia massa. Cras volutpat odio quis mi vulputate, id laoreet massa blandit. Morbi a consequat risus. Curabitur sollicitudin et diam at lacinia.
-
-Nunc congue eget risus a semper. Suspendisse egestas vulputate urna, sit amet fermentum mi efficitur in. Nulla ut nulla libero. Donec consequat placerat metus sit amet dapibus. Proin massa magna, pellentesque id luctus ut, rhoncus sed lacus. Donec fringilla imperdiet nulla, aliquet gravida erat lacinia et. Donec in commodo lectus, at ullamcorper dolor. Sed aliquet, justo non tincidunt pretium, quam mauris vulputate risus, quis euismod nunc odio non nibh.
-</p>
-    </div>
+    <div className="row home-padding">
+        <div className="col-md-2">
+            <p className="rounded yellow-button">0</p>
+            <label htmlFor="yellow-button" className="button-label yellow-label">Your teams</label>
+        </div>
+        <div className="col-md-2">
+            <p className="rounded blue-button">0</p>
+            <label htmlFor="blue-button" className="button-label blue-label">Your teams</label>
+        </div>
+        <div className="col-md-2">
+            <p className="rounded red-button">0</p>
+            <label htmlFor="red-button" className="button-label red-label">Your teams</label>
+        </div>
+    </div >
 )
 export default Home;
