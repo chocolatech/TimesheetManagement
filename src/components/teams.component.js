37d795ce1e89aa7944612ca6067905634a0951d7
ewelina <evelinagonda@gmail.com>
2017-02-21 13:51:59 +0100

    add teams table stub

--- src/components/teams.component.js
+++ src/components/teams.component.js
@@ -1,10 +1,56 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../styles/sideNav.css';
+import '../styles/teams.css';
 
 export default class Teams extends Component {
-    render(){
+    render() {
         return (
-            <p>loool</p>
+            <div>
+                <h3 className="blue-font">Your teams</h3><br />
+                <div>
+                    <table className="table table-striped table-hover">
+                        <thead className="table-header">
+                            <tr>
+                                <th>#</th>
+                                <th>Team</th>
+                                <th>Cloud</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         )
     }
 }
